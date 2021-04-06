e878592c8bcb1ec7cbfb6e961902bc0f4d8b4b28
antonioJRN <netinhorego13@gmail.com>
2021-04-06 01:39:47 -0300

    structureCompleted

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
